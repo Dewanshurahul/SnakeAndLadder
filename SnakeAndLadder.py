9ca8187ca4ad4ecc30a4be2b5b4f15905a8c149e
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-22 15:10:20 +0530

    [Dewanshu Rahul]Adding: Checking option and Playing Accordingly.

--- SnakeAndLadder.py
+++ SnakeAndLadder.py
@@ -1,5 +1,17 @@
 import random
+
 print("----Let's Gooo----")
+# The Player will Start from position 0
 position = 0
 playerName = input("Player Please Enter Your Name: ")
-die_value = (((random.random()*100) // 10) % 6)+ 1
+# A random Number is Generated and according to this the Player will move Forward or Back Ward
+die_value = (((random.random() * 100) // 10) % 6) + 1
+#Random Number is generated to check whether Player will Play or not.
+play_Condition = ((random.random() * 100) // 10) % 3
+print(play_Condition)
+if play_Condition == 0:  # Condition is True for NO Play (The Player will be remain at the Same Point)
+    pass
+elif play_Condition == 1:  # Condition is True for Ladder
+    position += die_value
+elif play_Condition == 2:  #Condition is True for Snake
+    position -= die_value
