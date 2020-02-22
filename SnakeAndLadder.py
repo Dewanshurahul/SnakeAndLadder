ca8fc19428843dac3c3d608f216161d4c01311d1
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-22 16:32:20 +0530

    [Dewanshu Rahul]Adding: Functionality to show Dice Throw And Position

--- SnakeAndLadder.py
+++ SnakeAndLadder.py
@@ -4,21 +4,27 @@ print("----Let's Gooo----")
 # The Player will Start from position 0
 position = 0
 playerName = input("Player Please Enter Your Name: ")
+# Used to Count the Number of times the Dice is thrown
+dice_thow = 0
 # Until the Player Wins the Game it will be Continued
 while position < 100:
+    # A random Number is Generated and according to this the Player will move Forward or Backward
+    die_value = (((random.random() * 100) // 10) % 6) + 1
+    # Count Every Dice Throw
+    dice_thow += 1
+    #Random Number is generated to check whether Player will Play or not.
+    play_Condition = ((random.random() * 100) // 10) % 3
+    if play_Condition == 0:  # Condition is True for NO Play (The Player will be remain at the Same Point)
+        continue
+    # Condition is True for Ladder and ensure exact Winning Condition
+    elif play_Condition == 1 and position + die_value <= 100:
+        position += die_value
+    # Condition is True for Snake
+    elif play_Condition == 2:
+        position -= die_value
     # In case the player position moves below 0
     # then the player restarts from 0
     if position < 0:
         position = 0
-    # A random Number is Generated and according to this the Player will move Forward or Back Ward
-    die_value = (((random.random() * 100) // 10) % 6) + 1
-    #Random Number is generated to check whether Player will Play or not.
-    play_Condition = ((random.random() * 100) // 10) % 3
-    print(play_Condition)
-    if play_Condition == 0:  # Condition is True for NO Play (The Player will be remain at the Same Point)
-        pass
-    elif play_Condition == 1 and position + die_value <= 100:  # Condition is True for Ladder
-        position += die_value
-    elif play_Condition == 2:  # Condition is True for Snake
-        position -= die_value
-    print(position)
+    print("Dice Count: "+str(dice_thow))
+    print("Position: "+str(position))
